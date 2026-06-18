aa0452730dba5641a87373885d7e1c3a0e59e77d
aadityansha06 <pkumariverma42@gmail.com>
2026-06-18 21:26:01 +0530

    formating

--- matrix.c
+++ matrix.c
@@ -1,17 +1,18 @@
-#define _POSIX_C_SOURCE 199309L // Required for CLOCK_MONOTONIC to measure precise timer without
-          // moving backward
+#define _POSIX_C_SOURCE 199309L
+// Required for CLOCK_MONOTONIC to measure precise timer without
+// moving backward
+#include <omp.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <omp.h>
 
 #define Total_SIZE 1024 * 1024
 
 void multiply(int n, const double *restrict A, const double *restrict B,
               double *restrict C) {
 
-  #pragma omp parallel for // from 1.2 sec to 0.607 sec
+#pragma omp parallel for // from 1.2 sec to 0.607 sec
   for (int i = 0; i < n; i++) {
     for (int k = 0; k < n; k++) {
       for (int j = 0; j < n; j++) {
@@ -35,7 +36,8 @@ int main() {
     A[i] = 2.0;
     B[i] = 3.0;
   }
-    printf("\n\tNaive Matrix multiplication with max compiler optimization,Loop re-order and distributed across multiple core\n");
+  printf("\n\tNaive Matrix multiplication with max compiler optimization,Loop "
+         "re-order and distributed across multiple core\n");
 
   printf("\nStarting benchmark for %d x %d matrix...\n", n, n);
   clock_gettime(CLOCK_MONOTONIC, &start);
@@ -48,9 +50,9 @@ int main() {
   printf("\nFunction took %f seconds to execute.\n", time_taken);
 
   // Calcuating GFLOPS for i3-2nd gen machine with theoratical DP GFLOPS as 52.8
-    double operations = 2.0 * (double)n * (double)n * (double)n;
+  double operations = 2.0 * (double)n * (double)n * (double)n;
 
-  double achived_gflpos = operations/ (time_taken * 1e9);
+  double achived_gflpos = operations / (time_taken * 1e9);
   double theoretical_gflops = 52.8;
 
   double efficiency = (achived_gflpos / theoretical_gflops) * 100.0;
@@ -58,7 +60,7 @@ int main() {
   printf(
       "\n Achived GFLOPS: %f\n Theoretical DP GFLOPS: %f\n Efficiency %f%% \n ",
       achived_gflpos, theoretical_gflops, efficiency);
-printf("Verification: C[0] = %f\n\n", C[0]);
+  printf("Verification: C[0] = %f\n\n", C[0]);
   free(A);
   free(B);
   free(C);
